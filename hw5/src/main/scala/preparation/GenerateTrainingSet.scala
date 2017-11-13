95c3f17d443722fd35fdc66dc131966e0cdc528f
xlabcba <xie.li@husky.neu.edu>
2017-11-13 18:45:00 -0500

    Changed to broadcast image map

--- hw5/src/main/scala/preparation/GenerateTrainingSet.scala
+++ hw5/src/main/scala/preparation/GenerateTrainingSet.scala
@@ -12,7 +12,7 @@ import org.apache.spark.sql.SaveMode
 
 import fileLoader.LoadMultiStack
 
-object GenerateTrainingSet {
+object GenerateTrainingSet {      
   
   def toMatrixCoord(index: Int) : (Int, Int, Int) = {
     val xDim = 512; val yDim = 512; val zDim = 60;
@@ -105,37 +105,37 @@ object GenerateTrainingSet {
     res.toList
   }
   
-  def emitNeighborPixels(z: Int, stackMap: Map[Int, Array[Byte]]) : List[(Int, List[Byte])] = {
-    val (xNum, yNum, zNum) = (3, 3, 3)
-    val (xDim, yDim, zDim) = (512, 512, 60)
-    val res = ListBuffer.empty[(Int, List[Byte])]
-    val offset = z * xDim * yDim 
-    for (i <- 0 until yDim * xDim) {
-      val pixel = ListBuffer.empty[Byte]
-      val neighborLst = findNeighbors(i)
-      if (neighborLst.length == xNum * yNum) {
-        for (dz <- -(zNum / 2) to (zNum / 2)) {
-          for (neighbor <- neighborLst) {
-            val brightness = stackMap(z + dz)(neighbor)
-            pixel += brightness
-          }
-        }
-        val curRes = (i + offset, pixel.toList)
-        res += curRes
-      }
-    }
-    res.toList
-  }
+//  def emitNeighborPixels(z: Int, stackMap: Map[Int, Array[Byte]]) : List[(Int, List[Byte])] = {
+//    val (xNum, yNum, zNum) = (3, 3, 3)
+//    val (xDim, yDim, zDim) = (512, 512, 60)
+//    val res = ListBuffer.empty[(Int, List[Byte])]
+//    val offset = z * xDim * yDim 
+//    for (i <- 0 until yDim * xDim) {
+//      val pixel = ListBuffer.empty[Byte]
+//      val neighborLst = findNeighbors(i)
+//      if (neighborLst.length == xNum * yNum) {
+//        for (dz <- -(zNum / 2) to (zNum / 2)) {
+//          for (neighbor <- neighborLst) {
+//            val brightness = stackMap(z + dz)(neighbor)
+//            pixel += brightness
+//          }
+//        }
+//        val curRes = (i + offset, pixel.toList)
+//        res += curRes
+//      }
+//    }
+//    res.toList
+//  }
   
-  def findNeighbors(i: Int) : List[Int] = {
-    val (xNum, yNum, zNum) = (3, 3, 3)
-    val (xDim, yDim, zDim) = (512, 512, 60)
+  def findNeighbors(i: Int, neighborSize: (Int, Int, Int), imageSize: (Int, Int, Int)) : List[Int] = {
+    val (xNum, yNum, zNum) = neighborSize
+    val (xDim, yDim, zDim) = imageSize
     val (x, y, z) = toMatrixCoord(i)
     val res = ListBuffer.empty[Int]
     for (dx <- -(xNum / 2) to (xNum / 2)) {
       for (dy <- -(yNum / 2) to (yNum / 2)) {
         val (nx, ny) = (x + dx, y + dy)
-        if (isValidPixel(nx, ny, 0)) {
+        if (isValidPixel(nx, ny, 0, imageSize)) {
           res += toPlaneIdx(nx, ny)
         }
       }
@@ -143,13 +143,16 @@ object GenerateTrainingSet {
     res.toList
   }
   
-  def isValidPixel(x: Int, y: Int, z: Int) : Boolean = {
-    val (xDim, yDim, zDim) = (512, 512, 60)
+  def isValidPixel(x: Int, y: Int, z: Int, imageSize: (Int, Int, Int)) : Boolean = {
+    val (xDim, yDim, zDim) = imageSize
     x >= 0 && x < xDim && y >= 0 && y < yDim && z >= 0 && z < zDim
   }
   
-  def isOnEdge(i: Int) : Boolean = {
-    val (xDim, yDim, zDim) = (512, 512, 60)
+  def isValidStack(stackIdx: Int, zNum: Int, zDim: Int): Boolean = {
+    stackIdx >= zNum / 2 && stackIdx < zDim - zNum / 2
+  }
+  
+  def isOnEdge(i: Int, xDim: Int, yDim: Int, zDim: Int) : Boolean = {
     val (x, y, z) = toMatrixCoord(i)
     x == 0 || x == (xDim - 1) || y == 0 || y == (yDim - 1) || z == 0 || z == (zDim - 1)
   }
@@ -159,8 +162,10 @@ object GenerateTrainingSet {
     // Parse args
     // val input = args(0)
     // val output = args(1)
-    val inputImage = "input/1_image.tiff"
-    val inputDist = "input/1_dist.tiff"
+    val inputImages = Array("input/1_image.tiff", "input/2_image.tiff", "input/3_image.tiff", "input/4_image.tiff", "input/6_image.tiff")
+    val inputDists = Array("input/1_dist.tiff", "input/2_dist.tiff", "input/3_dist.tiff", "input/4_dist.tiff", "input/6_dist.tiff")
+    val sizes = Array((512, 512, 60), (512, 512, 33), (512, 512, 44), (512, 512, 51), (512, 512, 46))
+    val neighborSize = (3, 3, 3)
     val output = "output"
     
     //Start the Spark context
@@ -174,33 +179,46 @@ object GenerateTrainingSet {
     val sqlContext= new org.apache.spark.sql.SQLContext(sc)
     import sqlContext.implicits._
     
-    val imageArray = LoadMultiStack.loadImage(inputImage, 512, 512, 60)
-    val imageDF = sc.parallelize(imageArray).zipWithIndex
-    .flatMap{case(stackMatrix, z) => emitNeighborPlanes(z, stackMatrix)}
-    .reduceByKey(_ ::: _)
-    .filter{case(z, stackLst) => stackLst.length == 3}
-    .mapValues(_.toMap)
-    .flatMap{case(z, stackMap) => emitNeighborPixels(z, stackMap)}
-    .toDF("index", "brightness")
-    // .persist(StorageLevel.MEMORY_AND_DISK)
+    val imageRDD = (0 until inputImages.length).map(i => {
+      val array = LoadMultiStack.loadImage(inputImages(i), sizes(i)._1, sizes(i)._2, sizes(i)._3)
+      sc.parallelize(array).zipWithIndex.map{case(stackArray, stackIdx) => ((i, stackIdx.toInt), stackArray)}
+    })
+    .reduce(_ union _)
     
-    // imageDF.show()
+    val imageMap = sc.broadcast(imageRDD.collectAsMap)
+        
+    val trainingRecords = (0 until inputDists.length).map(i => {
+      val array = LoadMultiStack.loadImage(inputDists(i), sizes(i)._1, sizes(i)._2, sizes(i)._3)
+      sc.parallelize(array).zipWithIndex.map{case(stackArray, stackIdx) => ((i, stackIdx.toInt), stackArray)}.repartition(sizes(i)._3)
+    })
+    .reduce(_ union _)
+    .filter{case((imageIdx, stackIdx), stackArray) => isValidStack(stackIdx, neighborSize._3, sizes(imageIdx)._3)}
+    .flatMap{case((imageIdx, stackIdx), stackArray) => {
+      val (xNum, yNum, zNum) = neighborSize
+      val (xDim, yDim, zDim) = sizes(imageIdx)
+      val res = ListBuffer.empty[(Int, Int, Byte, List[Byte])]
+      val offset = stackIdx * xDim * yDim 
+      for (i <- 0 until yDim * xDim) {
+        val pixel = ListBuffer.empty[Byte]
+        val neighborLst = findNeighbors(i, neighborSize, sizes(imageIdx))
+        if (neighborLst.length == xNum * yNum) {
+          for (dz <- -(zNum / 2) to (zNum / 2)) {
+            for (neighbor <- neighborLst) {
+              val brightness = imageMap.value(imageIdx, stackIdx + dz)(neighbor)
+              pixel += brightness
+            }
+          }
+          val curRes = (imageIdx, i + offset, stackArray(i), pixel.toList)
+          res += curRes
+        }
+      }
+      res.toList
+    }}
+    .map{case(imageIdx, pixelIdx, distance, brightnessLst) => (imageIdx, pixelIdx, toLabel(distance), brightnessLst)}
+    .filter{case(imageIdx, pixelIdx, label, brightnessLst) => label != -1}
+    .toDF("imageIndex", "pixelIndex", "label", "neighborBrightness")
     
-    val distArray = LoadMultiStack.loadDist(inputDist, 512, 512, 60)
-    val labelDF = sc.parallelize(distArray).zipWithIndex
-    .map{case(distance, index) => (index.toInt, toLabel(distance))}
-    .filter{case(index, label) => label != -1 && !isOnEdge(index)}
-    .toDF("index", "label")
-    // .persist(StorageLevel.MEMORY_AND_DISK)
-    
-    // labelDF.show()
-    
-    val trainingSet = imageDF.join(broadcast(labelDF), "index")
-    
-    trainingSet.show()
-    //trainingSet.write.format("org.apache.spark.sql.json").mode(SaveMode.Append).save(output)
-    //trainingSet.write.json(output)
-//    
+    trainingRecords.show() 
     Thread.sleep(1000000000)
   }
 }
